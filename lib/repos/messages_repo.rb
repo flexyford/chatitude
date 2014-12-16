3aa3cea24c32d0997df00682ddf6e6ac9e5e7505
Michael Bashour <michael.s.bashour@gmail.com>
2014-12-16 15:32:51 -0600

    Update MessagesRepo::Message:save with message_data and test for blank messages.

--- lib/repos/messages_repo.rb
+++ lib/repos/messages_repo.rb
@@ -1,11 +1,13 @@
 module MessagesRepo
 	class Message
-		def self.save db, user_data
-			if user_data['id']
-				result = db.exec("UPDATE message SET message = $2 WHERE userId = $1", [user_data['id'], user_data['message']])
+		def self.save db, message_data
+			if message_data['id']
+				result = db.exec("UPDATE message SET message = $2 WHERE userId = $1", [message_data['id'], message_data['message']])
+			elsif message_data['message'] = ""
+				raise ("Message text cannot be blank")
 			else
 				sql = %Q[INSERT INTO messages (userId, message) VALUES ($1, $2)]
-				db.exec(sql, [user_data['id'], user_id['message']])
+				db.exec(sql, [message_data['user_id'], message_data['message']])
 		end
 	end
 end
