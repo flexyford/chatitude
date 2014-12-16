c9bb3e5c07d14ed173949f4fc97dc8960d006561
Glenn Meyer <glennmeyer@gmail.com>
2014-12-16 22:09:05 +0000

    add time column

--- lib/repos/messages_repo.rb
+++ lib/repos/messages_repo.rb
@@ -3,14 +3,14 @@ module Chatitude
 
     def self.all db
       result = db.exec <<-SQL
-        SELECT u.username, m.message
+        SELECT u.username, m.time, m.message
         FROM users u
         JOIN messages m
         ON m."userId" = u.id
       SQL
       result.entries
     end
-    
+
 		def self.save db, message_data
 			if message_data['id']
 				result = db.exec("UPDATE message SET message = $2 WHERE userId = $1", [message_data['id'], message_data['message']])
