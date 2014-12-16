1874ebed1e02e9aca8cff11bf7053d7fe473e91a
Michael Bashour <michael.s.bashour@gmail.com>
2014-12-16 15:00:31 -0600

    Modify send_message:  Rename to self.save; follow 'save' pattern

--- lib/repos/messages_repo.rb
+++ lib/repos/messages_repo.rb
@@ -1,8 +1,11 @@
 module MessagesRepo
 	class Message
-		def self.send_message user_id, msg
-			sql = %Q[INSERT INTO messages (message, user_id) VALUES ($1, $2)]
-			db.exec(sql, [msg, user_id])
+		def self.save db, user_data
+			if user_data['id']
+				result = db.exec("UPDATE message SET message = $2 WHERE userId = $1", [user_data['id'], user_data['message']])
+			else
+				sql = %Q[INSERT INTO messages (userId, message) VALUES ($1, $2)]
+				db.exec(sql, [user_data['id'], user_id['message']])
 		end
 	end
 end
