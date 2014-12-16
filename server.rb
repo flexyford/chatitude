68361c082bac2b75efac22c6958d530b49eba218
Michael Bashour <michael.s.bashour@gmail.com>
2014-12-16 11:54:02 -0600

    Basic server.rb skeleton and Gemfile

--- server.rb
+++ server.rb
@@ -1,3 +1,30 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'rest-client'
+require 'json'
+require 'pg'
+
 get '/' do
   send_file 'public/index.html'
 end
+
+get '/chats' do 
+
+end
+
+post '/signup' do
+
+end
+
+post '/signin' do
+
+end
+
+post '/chats' do
+
+end
+
+delete '/signout' do
+	session.clear
+end
+
